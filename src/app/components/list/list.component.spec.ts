be21e5a0b86ad07f7ad21083ca0f5f5cd48ce973
Erika Faustino <dossantos.erika08@gmail.com>
2023-05-10 17:50:13 -0300

    Update list.component.spec.ts

--- src/app/components/list/list.component.spec.ts
+++ src/app/components/list/list.component.spec.ts
@@ -6,6 +6,16 @@ describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
 
+    
+    //teste de componente
+     it('deve renderizar o componente corretamente', () => {
+    expect(component).toBeTruthy();
+    const titulo = fixture.nativeElement.querySelector('h1');
+    expect(titulo.textContent).toContain('PokemonPage')
+    });
+    
+    
+    
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ ListComponent ]
